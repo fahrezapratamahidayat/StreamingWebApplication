b133a00e97aa90c7c585776fa76e49bba2612674
fahrezapratamh <fahrezaxgamer@gmail.com>
2023-12-18 17:42:29 +0700

    fix: api

--- src/services/DataApi.ts
+++ src/services/DataApi.ts
@@ -45,8 +45,6 @@ export const FetchingData = async (parameter: string) => {
     const res = await fetch(requestOptions.url, {
       method: requestOptions.method,
       headers: requestOptions.headers,
-      // cache: 'no-store',
-      // next: { revalidate: 10 },
     });
     if (!res.ok) {
       throw new Error(`Failed to fetch data. Status: ${res.status}`);
