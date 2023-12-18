afa6bd8465fcd97b4f71ab70433df0cb277deab6
fahrezapratamh <fahrezaxgamer@gmail.com>
2023-12-18 18:55:23 +0700

    fix : api revalidate and cache

--- src/services/DataApi.ts
+++ src/services/DataApi.ts
@@ -44,7 +44,11 @@ export const FetchingData = async (parameter: string) => {
   try {
     const res = await fetch(requestOptions.url, {
       method: requestOptions.method,
-      headers: requestOptions.headers
+      headers: requestOptions.headers,
+      cache: 'no-store',
+      next: {
+        revalidate: 1,
+      }
     });
     if (!res.ok) {
       throw new Error(`Failed to fetch data. Status: ${res.status}`);
