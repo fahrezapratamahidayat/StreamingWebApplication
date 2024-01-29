8bbdc32b69a907dc11e262b51963761f93966c8c
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-01-29 14:43:37 +0700

    fix margin left for skeleton

--- src/components/skeleton/SkeletonCard.tsx
+++ src/components/skeleton/SkeletonCard.tsx
@@ -9,7 +9,7 @@ export default function SkeletonCard({dummyArray}: {dummyArray: number}) {
     <>
       <div className={`animate-pulse`}>
         <div className={`mt-2 w-[130px] h-5 bg-slate-800 `}></div>
-        <div className={`lg:flex flex-wrap grid md:flex grid-cols-3 ${showNavbar ? "lg:gap-5" : "lg:gap-4"} md:gap-1 mt-[18px] min-[400px]:flex max-[767px]:flex min-[400px]:gap-1 max-[767px]:gap-1 min-[400px]:ml-4 lg:ml-0`}>
+        <div className={`lg:flex flex-wrap grid md:flex grid-cols-3 ${showNavbar ? "lg:gap-5" : "lg:gap-4"} md:gap-1 mt-[18px] min-[400px]:flex max-[767px]:flex min-[400px]:gap-1 max-[767px]:gap-1 min-[400px]:ml-4 lg:ml-0 ml-2`}>
           {Array.from({ length: dummyArray }).map((movie: any, index: number) => (
             <div
               className="flex flex-col justify-center items-start gap-[5px]"
