18e614fa01ece6fe6c11955c90e7da6c07d106df
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-02-01 18:34:13 +0700

    fix components list

--- src/app/mylist/page.tsx
+++ src/app/mylist/page.tsx
@@ -17,7 +17,7 @@ export default function WatchListPage() {
         `http://localhost:3000/api/user?id=${session.user.id}`
       );
       const data = await snapshot.json();
-      setUserWatchList(data.user.watchlist);
+      setUserWatchList(data.watchlist);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -34,7 +34,7 @@ export default function WatchListPage() {
         <div className="mt-[9rem] lg:mt-[5rem] mx-2 lg:mx-5">
           <h2 className="text-white font-semibold text-[20px] lg:pl-0 pl-2">Watchlist</h2>
           <div className="grid lg:grid-cols-11 grid-cols-3 lg:mx-0 mx-2 gap-2 lg:gap-[26px] mt-[18px]">
-            {userWatchList.map((data: any, index: number) => (
+            {userWatchList && userWatchList.map((data: any, index: number) => (
               <CardPosterWatchList key={index} data={data} index={index} />
             ))}
           </div>
