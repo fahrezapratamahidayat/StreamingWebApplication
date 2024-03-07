660cad888131db83ef1383c1d32484814bfcd24f
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-03-07 18:01:17 +0700

    add wavy background

--- src/components/views/HomePage.tsx
+++ src/components/views/HomePage.tsx
@@ -1,22 +1,21 @@
 import NavbarFixed from "../navbar/NavbarFixed";
+import { WavyBackground } from "../ui/wavy-background";
 
 export default function HomePageView() {
-
   return (
     <>
-    <NavbarFixed />
-      <div className="min-w-full min-h-screen ">
-        <div className="absolute top-0 left-0 h-full w-full flex items-center justify-center">
-          <h1
-            className={`font-bold lg:text-7xl text-5xl text-white tracking-widest relative z-10`}
-          >
-            Santai{" "}
-            <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-violet-500">
-              Movies
-            </span>
-          </h1>
+      <NavbarFixed />
+      <WavyBackground>
+        <div className="">
+          <div className="flex items-center justify-center">
+            <h1
+              className={`font-bold lg:text-7xl text-5xl text-white tracking-widest relative z-10`}
+            >
+              Santai Movies
+            </h1>
+          </div>
         </div>
-      </div>
+      </WavyBackground>
     </>
   );
 }
