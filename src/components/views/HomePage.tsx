f47b08131ec218f71ea178b60eb728868900b448
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-03-09 16:14:28 +0700

    change speed image slider to fast

--- src/components/views/HomePage.tsx
+++ src/components/views/HomePage.tsx
@@ -9,16 +9,7 @@ export default async function HomePageView() {
   return (
     <>
       <NavbarFixed />
-      <HeroParallax movies={movies} tv={tv} speed="slow" direction="left" />
-        {/* <div className="">
-          <div className="flex items-center justify-center">
-            <h1
-              className={`font-bold lg:text-7xl text-5xl text-white tracking-widest relative z-10`}
-            >
-              Santai Movies
-            </h1>
-          </div>
-        </div> */}
-    </>
+      <HeroParallax movies={movies} tv={tv} speed="fast" direction="left" />
+      </>
   );
 }
