df196a79cf5bfca2c14774d848d923175531f2af
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-03-10 11:48:17 +0700

    remove props speed direction

--- src/components/views/HomePage.tsx
+++ src/components/views/HomePage.tsx
@@ -1,15 +1,14 @@
-import {fetchTrendingAll } from "@/app/actions";
+import { fetchTrendingAll } from "@/app/actions";
 import NavbarFixed from "../navbar/NavbarFixed";
 import { HeroParallax } from "../ui/hero-parallax";
 
 export default async function HomePageView() {
   const movies = await fetchTrendingAll(`movie/upcoming`);
   const tv = await fetchTrendingAll(`tv/top_rated`);
-
   return (
     <>
       <NavbarFixed />
-      <HeroParallax movies={movies} tv={tv} speed="fast" direction="left" />
-      </>
+      <HeroParallax movies={movies} tv={tv} />
+    </>
   );
 }
