ed97bb2cf6b9aedb112e61b3a8b5997f9475ea95
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-03-10 11:20:17 +0700

    change name metada and add description about santai

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
-
 import HomePageView from "@/components/views/HomePage";
 import { Metadata } from "next";
 
-export const metadata : Metadata = {
-  title: "Santai Wir",
-  description: "SantaiWir",
-}
+export const metadata: Metadata = {
+  title: "Santai",
+  description: `Selamat Datang Di Santai disini kami menyediakan berbagai Film dan Tv
+  Series Yang berkualitas dan seru-seru pokonya!. Santai ini dibuat oleh
+  seorang develover yang awalnya cuma iseng belajar tentang dunia web
+  develover dan belajar Api. karena dari ke isenganya tersebut develover
+  dapat membuat website ini`,
+};
 
-export default async function HomePage(){
+export default async function HomePage() {
   return (
     <>
       <HomePageView />
     </>
-  )
+  );
 }
