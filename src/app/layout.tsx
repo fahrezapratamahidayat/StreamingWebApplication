42f2bec5663fd0b619dcc95bd1d779e2be20d588
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-03-11 16:12:09 +0700

    add tag meta refresh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
     <html lang="en" className="">
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta http-equiv="refresh" />
       <body className={`${inter.className} overflow-main bg-black`}>
         <NextTopLoader
           color="#fff"
