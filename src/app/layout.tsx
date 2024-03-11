f96afe8180e1773ecdc89b1af65a94d3d496340a
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-03-11 16:28:46 +0700

    add meta title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta http-equiv="refresh" />
+      <title>Santai</title>
       <body className={`${inter.className} overflow-main bg-black`}>
         <NextTopLoader
           color="#fff"
